0df84d9483ed2fbd433c9dd81dda0bf98bdd6870
Andrew Mccloud <02andrew.mccloud@gulllakecs.org>
2022-02-24 13:55:33 -0500

    Declared vehicleResponseSystem-IF- Stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -10,4 +10,9 @@ def weather():
 # Calling weather Function to determine weather
 weatherAlert = weather()
 
-print(weatherAlert)
+def vehicleResponseSystem():
+    if weatherAlert == "Icy":
+        print("\nVRS has changed your Alar 30 minutes earlier based on the NWS forecast of",weatherAlert)
+        print("VRS will only allow your car to go 30MPH")
+
+vehicleResponseSystem()
